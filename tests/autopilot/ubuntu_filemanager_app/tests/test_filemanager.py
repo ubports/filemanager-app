f9abdd88e4f15f6d4b2355d8446889ecab63b62e
Leo Arias <leo.arias@canonical.com>
2013-07-25 14:19:34 -0600

    Removed unused import.

--- tests/autopilot/ubuntu_filemanager_app/tests/test_filemanager.py
+++ tests/autopilot/ubuntu_filemanager_app/tests/test_filemanager.py
@@ -18,7 +18,6 @@ import shutil
 
 from autopilot.matchers import Eventually
 from testtools.matchers import Equals
-#from testtools.matchers import NotEquals
 
 from ubuntu_filemanager_app.tests import FileManagerTestCase
 
