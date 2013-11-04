212d244bbe89e712d8df21dc492580755d9703e9
chris.gagnon <chris.gagnon@canonical.com>
2013-11-04 18:01:48 -0500

    use base.get_launch_command to get qmlscene from ubuntu-ui-toolkit

--- tests/autopilot/ubuntu_filemanager_app/tests/__init__.py
+++ tests/autopilot/ubuntu_filemanager_app/tests/__init__.py
@@ -16,8 +16,12 @@ from autopilot.input import Mouse, Touch, Pointer
 from autopilot.platform import model
 from autopilot.testcase import AutopilotTestCase
 
-from ubuntuuitoolkit import emulators as toolkit_emulators
 from ubuntu_filemanager_app import emulators
+
+from ubuntuuitoolkit import (
+    base,
+    emulators as toolkit_emulators
+)
 
 
 class FileManagerTestCase(AutopilotTestCase):
@@ -75,14 +79,14 @@ class FileManagerTestCase(AutopilotTestCase):
 
     def launch_test_local(self):
         self.app = self.launch_test_application(
-            "qmlscene",
+            base.get_qmlscene_launch_command(),
             self.local_location,
             app_type='qt',
             emulator_base=toolkit_emulators.UbuntuUIToolkitEmulatorBase)
 
     def launch_test_installed(self):
         self.app = self.launch_test_application(
-            "qmlscene",
+            base.get_qmlscene_launch_command(),
             self.installed_location,
             "--desktop_file_hint="
             "/usr/share/applications/ubuntu-filemanager-app.desktop",
