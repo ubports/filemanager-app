4225eb40473903a2359d32ddac026d13d44769d2
nskaggs <nicholas.skaggs@canonical.com>
2014-04-14 16:54:31 -0400

    undo main.cpp changes

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -45,9 +45,11 @@ int main(int argc, char *argv[])
         qDebug() << "    -t|--tablet   If running on Desktop, start in a tablet sized window.";
         qDebug() << "    -h|--help     Print this help.";
         qDebug() << "    -I <path>     Give a path for an additional QML import directory. May be used multiple times.";
+        qDebug() << "    -q <qmlfile>  Give an alternative location for the main qml file.";
         return 0;
     }
 
+    QString qmlfile;
     for (int i = 0; i < args.count(); i++) {
         if (args.at(i) == "-I" && args.count() > i + 1) {
             QString addedPath = args.at(i+1);
@@ -56,6 +58,8 @@ int main(int argc, char *argv[])
                 addedPath.prepend(QDir::currentPath());
             }
             importPathList.append(addedPath);
+        } else if (args.at(i) == "-q" && args.count() > i + 1) {
+            qmlfile = args.at(i+1);
         }
     }
 
@@ -89,20 +93,18 @@ int main(int argc, char *argv[])
 
     view.engine()->setImportPathList(importPathList);
 
-    QString qmlfile;
-    const QString filePath = QLatin1String("qml/ubuntu-filemanager-app.qml");
-    QStringList paths = QStandardPaths::standardLocations(QStandardPaths::DataLocation);
-    paths.prepend(QCoreApplication::applicationDirPath());
-    Q_FOREACH (const QString &path, paths) {
-        QString myPath = path + QLatin1Char('/') + filePath;
-        if (QFile::exists(myPath)) {
-            qmlfile = myPath;
+    // load the qml file
+    if (qmlfile.isEmpty()) {
+        QStringList paths = QStandardPaths::standardLocations(QStandardPaths::DataLocation);
+        paths.prepend(".");
+
+        foreach (const QString &path, paths) {
+            QFileInfo fi(path + "/qml/ubuntu-filemanager-app.qml");
+            if (fi.exists()) {
+                qmlfile = path +  "/qml/ubuntu-filemanager-app.qml";
                 break;
             }
         }
-    // sanity check
-    if (qmlfile.isEmpty()) {
-        qFatal("File: %s does not exist at any of the standard paths!", qPrintable(filePath));
     }
     qDebug() << "using main qml file from:" << qmlfile;
     view.setSource(QUrl::fromLocalFile(qmlfile));
