bf6a644def28b89be6eca72d718e2fbae08980d1
Arto Jalkanen <ajalkane@gmail.com>
2014-08-13 23:30:40 +0300

    Fix plugin URI assert.

--- src/plugin/pamauthentication/pamauthentication_plugin.cpp
+++ src/plugin/pamauthentication/pamauthentication_plugin.cpp
@@ -23,7 +23,7 @@
 
 void BackendPlugin::registerTypes(const char *uri)
 {
-    Q_ASSERT(uri == QLatin1String(QUOTES(PLUGIN_URI)));
+    Q_ASSERT(uri == QLatin1String("com.ubuntu.PamAuthentication"));
 
     qmlRegisterType<PamAuthentication>(uri, 0, 1, "PamAuthentication");
 }
