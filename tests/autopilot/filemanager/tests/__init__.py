14819f595c208091c4f9ee8b1a2941c2d0a48279
nskaggs <nicholas.skaggs@canonical.com>
2014-10-10 17:44:39 -0400

    add subdir to have quasi sane testing env

--- tests/autopilot/filemanager/tests/__init__.py
+++ tests/autopilot/filemanager/tests/__init__.py
@@ -233,12 +233,16 @@ class FileManagerTestCase(BaseTestCaseWithPatchedHome):
 
     def setUp(self):
         super(FileManagerTestCase, self).setUp()
-        self.fakehome = os.getenv('HOME')
+        testdir = fm_fixtures.TemporaryDirectoryInDirectory(
+            os.getenv('HOME'))
+        self.useFixture(testdir)
+        self.testhome = testdir.path
+        logger.debug('Test home set to %s' % self.testhome)
         self.original_file_count = \
-            len([i for i in os.listdir(self.fakehome)
+            len([i for i in os.listdir(self.testhome)
                  if not i.startswith('.')])
         logger.debug('Directory Listing for HOME\n%s' %
-                     os.listdir(self.fakehome))
+                     os.listdir(self.testhome))
         logger.debug('File count in HOME is %s' % self.original_file_count)
         self.app = filemanager.Filemanager(self.launcher(), self.test_type)
 
@@ -252,16 +256,16 @@ class FileManagerTestCase(BaseTestCaseWithPatchedHome):
         if type_ != 'file' and type_ != 'directory':
             raise ValueError('Unknown content type: "{0}"', type_)
         if type_ == 'file':
-            temp_file = fm_fixtures.TemporaryFileInDirectory(self.fakehome)
+            temp_file = fm_fixtures.TemporaryFileInDirectory(self.testhome)
             self.useFixture(temp_file)
             path = temp_file.path
         else:
             temp_dir = fm_fixtures.TemporaryDirectoryInDirectory(
-                self.fakehome)
+                self.testhome)
             self.useFixture(temp_dir)
             path = temp_dir.path
         logger.debug('Directory Listing for HOME\n%s' %
-                     os.listdir(self.fakehome))
+                     os.listdir(self.testhome))
         self._assert_number_of_files(1)
         return path
 
